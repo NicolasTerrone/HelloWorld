c863ddb09122c81426618618cc79dcbaaee14321
Nicolas TERRONE <nicolasterrone@gmail.com>
2019-01-07 20:40:23 +0100

    Add oulets and hide images on application laoding

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,8 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var bgImageView: UIImageView!
+    @IBOutlet weak var logoImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        bgImageView.isHidden = true
+        logoImageView.isHidden = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
